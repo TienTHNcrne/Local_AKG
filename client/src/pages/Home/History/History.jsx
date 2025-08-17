9ba08eba6d54e9e75a4ef9818b0ff9fa87a6fa66
TienTHNcrne <tienguyen3541@gmail.com>
2025-08-17 23:56:07 +0700

    finish History

--- client/src/pages/Home/History/History.jsx
+++ client/src/pages/Home/History/History.jsx
@@ -1,64 +1,42 @@
-import React from "react";
+/** @format */
+
+import React, { useState } from "react";
 import styles from "./History.module.scss";
+import Before from "./Components/Before/Before";
+import After from "./Components/After/After";
 export default function History() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.head}>
-                <p>
-                    Ngày 12/6/2025, Quốc hội thông qua Nghị quyết 202/2025/QH15
-                    – sáp nhập tỉnh Kiên Giang vào tỉnh An Giang, từ đó mở ra kỷ
-                    nguyên phát triển đồng bộ và nâng tầm vị thế vùng Đồng bằng
-                    sông Cửu Long.
-                </p>
-            </div>
-            <div className={styles.merge}>
-                {" "}
-                <div className={styles.bet}>
-                    <h3>Trước sáp nhập</h3>
-                </div>
-                <div className={styles.content}>
-                    <div className={styles.AG}>
-                        <h4>An Giang</h4>
-                        <div className={styles.content0}>
-                            <p>sfsd</p>
-                        </div>
-                    </div>
-                    <div className={styles.KG}>
-                        <h4>Kiên Giang</h4>
-                        <div className={styles.content1}>
-                            <p>sdfs</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/*PROCESS */}{" "}
-            <div className={styles.process}>
-                {" "}
-                <div className={styles.bet}>
-                    <h3>qua trinh sáp nhập</h3>
-                </div>
-                <div className={styles.content}>
-                    <div className={styles.KG}>
-                        <div className={styles.content1}>
-                            <p>sdfs</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/*AFTER */}{" "}
-            <div className={styles.after}>
-                {" "}
-                <div className={styles.bet}>
-                    <h3>sau sáp nhập</h3>
-                </div>
-                <div className={styles.content}>
-                    <div className={styles.KG}>
-                        <div className={styles.content1}>
-                            <p>sdfs</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	const [choose, setChoose] = useState("before");
+	return (
+		<div className={styles.container}>
+			<p>
+				Ngày 12/6/2025, Quốc hội thông qua Nghị quyết 202/2025/QH15 –
+				sáp nhập tỉnh Kiên Giang vào tỉnh An Giang, từ đó mở ra kỷ
+				nguyên phát triển đồng bộ và nâng tầm vị thế vùng Đồng bằng sông
+				Cửu Long.
+			</p>
+			<div className={styles.header}>
+				<div className={styles.before}>
+					<h2
+						onClick={() => {
+							setChoose("before");
+						}}
+						className={choose === "before" ? styles.processing : ""}
+					>
+						Trước sáp nhập
+					</h2>
+				</div>
+				<div className={styles.after}>
+					<h2
+						onClick={() => {
+							setChoose("after");
+						}}
+						className={choose === "after" ? styles.processing : ""}
+					>
+						Sau sáp nhập
+					</h2>
+				</div>
+			</div>
+			{choose === "before" ? <Before /> : <After />}
+		</div>
+	);
 }
