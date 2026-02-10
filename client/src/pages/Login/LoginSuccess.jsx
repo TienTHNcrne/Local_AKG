b9292b9b8e72b0e087306b712a5629d5aeb8893c
TienTHNcrne <tienguyen3541@gmail.com>
2026-02-10 21:42:00 +0700

    test

--- client/src/pages/Login/LoginSuccess.jsx
+++ client/src/pages/Login/LoginSuccess.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 export default function LoginSuccess() {
     const navigate = useNavigate();
-
     useEffect(() => {
         const timer = setTimeout(() => {
             navigate("/", { replace: true });
