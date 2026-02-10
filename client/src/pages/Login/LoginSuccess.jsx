3d52b511ec3d721efb957d3f9f4043ff2b4b0db0
TienTHNcrne <tienguyen3541@gmail.com>
2026-02-11 00:07:02 +0700

    fix

--- client/src/pages/Login/LoginSuccess.jsx
+++ client/src/pages/Login/LoginSuccess.jsx
@@ -16,7 +16,10 @@ export default function LoginSuccess() {
                 // Lưu vào localStorage
                 localStorage.setItem("token", decoded.token);
                 localStorage.setItem("user", JSON.stringify(decoded.user));
-                localStorage.setItem("userid", decoded.user.userId);
+                localStorage.setItem(
+                    "userid",
+                    JSON.stringify(decoded.user.userId),
+                );
                 console.log("Login OK:", decoded.user.userId);
             } catch (err) {
                 console.error("Parse login data failed:", err);
