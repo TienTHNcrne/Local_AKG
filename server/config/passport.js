ae57258c9b800b11d6aaf2cb25f4674a2f3152e3
TienTHNcrne <tienguyen3541@gmail.com>
2026-02-10 14:29:58 +0700

    becoming fix

--- server/config/passport.js
+++ server/config/passport.js
@@ -18,7 +18,10 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+            callbackURL:
+                process.env.NODE_ENV === "production"
+                    ? "https://akg-rfjj.onrender.com/v1/api/auth/google/callback"
+                    : "http://localhost:8081/v1/api/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
